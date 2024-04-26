0754222798869969d3b603a9b73c2adaf9bedead
Ilyas Tahari <114678345+ilyastahari@users.noreply.github.com>
2024-04-25 22:55:21 -0400

    Add files via upload

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+    {
+        id: 1,
+        title: "Energy Drink Website",
+        description: "A website where users are able to go through a catalog of energy drinks and view detailed information about them, as well as create their own posts for people to see and interact with.",
+        image: "/images/projects/Energy.webp",
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/",
+      },
+
+    {
+    id: 2,
+    title: "Recipe Maker",
+    description: "A website which makes it easier for college students to post recipes and find easy recipes to make.",
+    image: "/images/projects/Recipe.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  
+  {
+    id: 3,
+    title: "Company Management Site",
+    description: "A company management site where employees are able to create accounts, join teams, add projects, and add user stories.",
+    image: "/images/projects/Company.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;

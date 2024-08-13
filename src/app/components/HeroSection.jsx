9f3e3c738c0174962946db7a1473174e623ca43e
Ilyas Tahari <114678345+ilyastahari@users.noreply.github.com>
2024-08-12 22:43:11 -0400

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500">
-              LABAS, I'm{" Ilyas!"}
+              Hi, I'm{" Ilyas!"}
             </span>
             <br></br>
             <TypeAnimation
